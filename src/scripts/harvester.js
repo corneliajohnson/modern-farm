580ac67854ada89cd52efdc1453665648d2e8a2f
Cornelia Johnson <corneliaj91@gmail.com>
2020-08-20 10:34:18 -0400

    added havester.js

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -2,6 +2,13 @@
 export const harvestPlants = (plantObj) => {
   const plantArr = []
   for (const plant of plantObj) {
+    if (Array.isArray(plant)) {
+      //get objects out of corn array
+      for (let j = 0; j < plant[0].output; j++) {
+        plantArr.push(plant[0])
+      }
+    }
+    //Add as many plant amount the plant objects to the array that the function returns.
     for (let i = 0; i < plant.output; i++) {
       plantArr.push(plant)
     }
