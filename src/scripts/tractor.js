9a9a0adde826ed3c326479da7584c2414e302433
Cornelia Johnson <corneliaj91@gmail.com>
2020-08-26 17:01:18 -0400

    changes for loop

--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -1,48 +1,42 @@
-import { addPlant } from './field.js'
-import { createAsparagus } from './seeds/asparagus.js'
-import { createCorn } from './seeds/corn.js'
-import { createPotato } from './seeds/potato.js'
-import { createSoybean } from './seeds/soybean.js'
-import { createSunflower } from './seeds/sunflower.js'
-import { createWheat } from './seeds/wheat.js'
+import { addPlant } from "./field.js";
+import { createAsparagus } from "./seeds/asparagus.js";
+import { createCorn } from "./seeds/corn.js";
+import { createPotato } from "./seeds/potato.js";
+import { createSoybean } from "./seeds/soybean.js";
+import { createSunflower } from "./seeds/sunflower.js";
+import { createWheat } from "./seeds/wheat.js";
 
-
-
-const asparagusSeed = createAsparagus()
-const cornSeed = createCorn()
-const potatoSeed = createPotato()
-const soybeanSeed = createSoybean()
-const sunflowerSeed = createSunflower()
-const wheatSeed = createWheat()
-
+const asparagusSeed = createAsparagus();
+const cornSeed = createCorn();
+const potatoSeed = createPotato();
+const soybeanSeed = createSoybean();
+const sunflowerSeed = createSunflower();
+const wheatSeed = createWheat();
 
 export const plantSeeds = (yearlyPlan) => {
-  for (let i = 0; i < yearlyPlan.length; i++) {
-    for (let j = 0; j < yearlyPlan[i].length; j++) {
-
+  for (const row of yearlyPlan) {
+    for (const seed of row) {
       //call each create plant function in yearlyPlan arrays
-      let plantName = yearlyPlan[i][j]
-      switch (plantName) {
-        case 'Asparagus':
-          addPlant(asparagusSeed)
+      switch (seed) {
+        case "Asparagus":
+          addPlant(asparagusSeed);
           break;
-        case 'Corn':
-          addPlant(cornSeed)
+        case "Corn":
+          addPlant(cornSeed);
           break;
-        case 'Potato':
-          addPlant(potatoSeed)
+        case "Potato":
+          addPlant(potatoSeed);
           break;
-        case 'Soybean':
-          addPlant(soybeanSeed)
+        case "Soybean":
+          addPlant(soybeanSeed);
           break;
-        case 'Sunflower':
-          addPlant(sunflowerSeed)
+        case "Sunflower":
+          addPlant(sunflowerSeed);
           break;
-        case 'Wheat':
-          addPlant(wheatSeed)
+        case "Wheat":
+          addPlant(wheatSeed);
           break;
       }
-
     }
   }
-}
+};
